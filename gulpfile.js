10390f2a2552030bdaaa14315d748137c5f30566
imilyutkin <ivan.milyutkin@gmail.com>
2014-10-13 18:20:13 +0300

    added index file

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,18 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    del = require('del'),
+    notify = require('gulp-notify');
+
+gulp.task('clean', [], function() {
+    del('app/build/index.html');
+    notify('Clean completed');
+});
+
+gulp.task('index', [], function() {
+    gulp.src('app/src/index.html')
+      .pipe(gulp.dest('app/build/'))
+      .pipe(notify('Index file updated'));
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('index');
+});
