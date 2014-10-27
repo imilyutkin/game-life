50c4ca289ced41189c7d651e6d78aae30a7a4082
Ivan Milyutkin <ivan.milyutkin@gmail.com>
2014-10-27 22:11:14 +0300

    heroku: fixed web.js

--- web.js
+++ web.js
@@ -1,19 +1,14 @@
 var express = require('express')
-var path = require('path')
 var app = express();
 
 app.use(express.static(__dirname + '/app/build'))
+app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function (req, res) {
   res.send('Content')
 })
 
-
-var server = app.listen(3000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
 })
+
